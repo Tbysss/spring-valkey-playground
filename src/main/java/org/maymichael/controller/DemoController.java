9f9cb1d3bd758bcf422825387d072aca721232b6
tbysss <maytby@gmail.com>
2023-07-22 12:17:29 +0200

    prepare for demo * what to show and why

---
+++ src/main/java/org/maymichael/controller/DemoController.java
@@ -0,0 +1,73 @@
+package org.maymichael.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.maymichael.services.DataService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+@RequiredArgsConstructor
+@Slf4j
+public class DemoController {
+
+    private final DataService dataService;
+
+
+    @PostMapping("/demo1")
+    public void saveWithRepository(){
+        // demo1 : save with spring repositories (jpa)
+        log.info("preparing demo 1 data...");
+
+        // note: problems with spring crud repositories (and the like)
+        // no partial update -> always READS the data first, then deletes it and saves it again
+        // advantage: if the hash changes by an application update, the values will be consistent with the new api
+        // easy to use, if familiar with spring JPA
+        // disadvantage: redundant reads and inserts, especially if we only want a partial update
+        // PROBLEM: because of the deletion, we CANNOT run getData and saveData in parallel!
+        // e.g. what can happen is:
+        // T1: saveData - RM hash -> hash not removed from redis repository
+        // T2: getData - get HASH -> no Data FOUND -> empty
+        // T1: saveData - PUT hash -> data inserted back into redis repository
+        // because repository.save is not thread safe (or the lettuce connection is not) it may happen, that we
+        // try to get data in between, just when its deleted and not inserted back yet
+        // => only option is to use locks then
+        // alternative: use keyValueAdapter with Partial Update, which does NOT delete the entire HASH
+    }
+
+    @PostMapping("/demo2")
+    public void saveWithKeyValueAdapter(){
+        // demo2: save with key value adapter
+
+        // advantage: more control
+        // disadvantage: still deletes entries and reinserts, if just using insert
+        // BUT supports PartialUpdate (demo4), which overcomes this problem)
+    }
+
+    @PostMapping("/demo3")
+    public void saveWithPipeline(){
+        // demo3: save with pipeline key value adapter
+
+        // advantage: much faster because of pipeline (does not wait for result, no delete)
+        // disadvantage: lots of safety checks will be removed, custom implementation, not part of spring data core
+        // should only be used for a batch insert, not to update existing keys!
+    }
+
+    @PostMapping("/demo4")
+    public void time(){
+        // demo4: run demo1, demo2 and demo3 multiple times and get average time spend
+        // overview of demo 1, demo 2 and demo 3
+    }
+
+    @PostMapping("/demo5")
+    public void partialUpdate(){
+        // demo5: partial update
+    }
+
+    @PostMapping("/demo6")
+    public void partialRead(){
+        // demo6: partial read
+    }
+}
---
+++ src/main/java/org/maymichael/services/DataService.java
@@ -0,0 +1,7 @@
+package org.maymichael.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataService {
+}
